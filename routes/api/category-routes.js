9eeb4dc4b3d25ed0a47a441930d7a16e3e284975
priyanka damodar <priyadamodar12@gmail.com>
2021-04-25 17:59:07 -0400

    added categories and model for table

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,67 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll().then((categories) => {
+    res.json(categories);
+  });
+
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((category) => {
+    res.json(category);
+  });
+
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Book.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      category_name: req.body.category_name
+    },
+    {
+      // Gets the books based on the isbn given in the request parameters
+      where: {
+        isbn: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      // Sends the updated book as a json response
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      isbn: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
+
 });
 
 module.exports = router;
